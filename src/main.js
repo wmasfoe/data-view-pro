531bc0ac3e8b6c7c2f82d5a65d2e941404886737
lijiaqi.defined <lijiaqi.defined@bytedance.com>
2022-02-04 03:27:47 +0800

    feat: vue2 版本支持composition-api & 初始化

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import CompositionApi from "@vue/composition-api";
+import * as echarts from "echarts";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(CompositionApi);
+
+Vue.prototype.$charts = echarts;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defineComponent, ref } from "@vue/composition-api";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default defineComponent({
+  name: "Home",
+  setup() {
+    const state = ref(null);
+
+    return {
+      state,
+    };
+  },
+});
 </script>
