d77910381d53151acd1b9b560d76514358107f40
dapao <lijq1103@163.com>
2022-02-10 17:15:25 +0800

    feat: test

--- src/hooks/useCharts.js
+++ src/hooks/useCharts.js
@@ -23,7 +23,7 @@ export default function useCharts(options) {
 
   // 更新 data 并刷新 chart
   function refreshChartForData(chartRef, data = []) {
-    // todo 现在只能更改一个 series 里面的 data，需要支持 series 里面所有 data 的替换
+    // todo 现在只能更改一个 series 里面的 data，需要支持 series 里面所有 data 的替换 now
     if (!Array.isArray(data)) {
       throw new TypeError(`data ${data} is not Array`);
     }
