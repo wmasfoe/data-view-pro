f3a1510209f82c39cbad053b22607a5b2453737c
unknown <lijq1103@163.com>
2022-03-05 15:52:56 +0800

    fix: code format

--- src/hooks/useCharts.js
+++ src/hooks/useCharts.js
@@ -1,23 +1,23 @@
-import { ref, onMounted, nextTick } from "@vue/composition-api";
-import * as echarts from "echarts";
+import { ref, onMounted, nextTick } from '@vue/composition-api'
+import * as echarts from 'echarts'
 
-const chartMap = new Map();
+const chartMap = new Map()
 
 // 更新 data 并刷新 chart
-export function refreshChartForData(chartRef, data = []) {
+export function refreshChartForData (chartRef, data = []) {
   if (!Array.isArray(data)) {
-    throw new TypeError(`data ${data} is not Array`);
+    throw new TypeError(`data ${data} is not Array`)
   }
-  const id = chartRef.value.id;
+  const id = chartRef.value.id
   if (chartMap.has(id)) {
     // 取出配置
-    const chartOptions = chartMap.get(id);
+    const chartOptions = chartMap.get(id)
     data.forEach((item, inx) => {
-      chartOptions.series[inx].data = item;
+      chartOptions.series[inx].data = item
     })
-    chartRef.value.setOption(chartOptions);
+    chartRef.value.setOption(chartOptions)
   } else {
-    throw new Error(`chart id ${id} not exist`);
+    throw new Error(`chart id ${id} not exist`)
   }
 }
 
@@ -25,22 +25,22 @@ export function refreshChartForData(chartRef, data = []) {
   使用这个 hook 对暴露的 chartDom 进行 ref 绑定，传入 echarts 配置项，会自动生成图表；
   如需使用图表实例请 await nextTick 之后使用暴露出的 chartRef
  */
-export default function useCharts(options) {
-  const chartDom = ref(null);
-  const chartRef = ref(null);
+export default function useCharts (options) {
+  const chartDom = ref(null)
+  const chartRef = ref(null)
 
   onMounted(async () => {
-    await nextTick();
-    chartRef.value = echarts.init(chartDom.value);
+    await nextTick()
+    chartRef.value = echarts.init(chartDom.value)
 
     // 记录图表与 options 之间的关系
-    chartMap.set(chartRef.value.id, options);
+    chartMap.set(chartRef.value.id, options)
 
-    chartRef.value.setOption(options);
-  });
+    chartRef.value.setOption(options)
+  })
 
   return {
     chartDom,
-    chartRef,
-  };
+    chartRef
+  }
 }
